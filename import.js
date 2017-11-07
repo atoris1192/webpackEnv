1e5f307b6a2057c76f7763f60e2de84adb0b7804
k.niuishi <atoris1192@gmail.com>
2017-11-07 12:52:27 +0900

    first commit

---
+++ import.js
@@ -0,0 +1,5 @@
+import { foo, bar, Baz } from './module.js'
+
+console.log(foo);
+bar()
+let baz = new Baz
---
+++ src/js/modules/module.js
@@ -0,0 +1,8 @@
+var foo = 'foo';
+function bar() {};
+class Baz{
+  baz() {}
+
+}
+
+export { foo, bar, Baz };
